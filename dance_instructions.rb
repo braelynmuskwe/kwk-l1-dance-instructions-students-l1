4d65f25cd8f6019bc1abac2cca42966e5d01c1e8
braelynmuskwe <braemira1@icloud.com>
2018-06-20 17:37:47 +0000

    automatically backed up by learn

--- dance_instructions.rb
+++ dance_instructions.rb
@@ -13,7 +13,8 @@ puts "lift your arms up and down in a loose bouncing movement, in time with the 
 puts "start arm movement by holding left arm so the back of your wrist is near your chin, pointing your left elbow straight left"
 puts "lift right arm up and point right elbow diagonally to the right"
 puts "raise right forearm so that it points straight up, and whip it in small circles to the beat of the song, as if you were a cowboy holding a rope lasso. this movement is repeated eight times as well."
-puts "Holding your arms out in front of you, straight and at chest level"
+puts "Holding your arms out in front of you, straight and at chest level Cross your right wrist over your left and hold them together
+Lift your arms up and down in a loose bouncing movement, in time with the beat of the song."
 
 end 
 dance_instructions
